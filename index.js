58319b4ed369c1f2714513703e7db396a0012e33
Trent112232 <69593392+Trent112232@users.noreply.github.com>
2020-11-22 19:21:21 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,90 @@
-////////////////////////////////////
-// Discord Bot Template by Trent! //
-// https://github.com/Trent112232 //
-////////////////////////////////////
-
-
-// If these dependencies don't work, or arent installed, open your terminal and run: npm install
-// Or just install them separately with: npm install [PACKAGE]
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const fs = require("fs");
+const ms = require("ms");
 const chalk = require('chalk');
-// Used a variable to define the token, it's only used at the bottom of the script so you can remove this if you like.
+
+
+
 const settings = {
-    botToken: "TOKEN HERE"
+    botToken: ""
 };
-const config = require("./config.json"); // The config stores the prefix, for commands. If you like, however, you can also add other things to the config.
-client.on("ready", () => { // The bot's presence
-    console.log(`Bot initialised!`); // Tells you when the bot's online
+
+const config = require("./config.json");
+client.on("ready", () => {
+    console.log(`Bot initialised`); 
     client.user.setPresence({
         game: {
-            name: "Discord Bot",
-            type: "PLAYING"
+            name: ";help | Made by Trent",
+            type: "Playing"
         }
     })
     })
 client.on("message", async message => {
-    if(message.author.bot) return;
-    if(!message.content.startsWith(config.prefix)) return;
-    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-        
-    if(command === "help") { // Here you can start adding commands!
-        message.channel.send({embed: {
-            color: 1146986, // The embed bar's colour.
-            title: "Bot", // The title of the embed.
-            description: "`help`" // The actual text in the embed.
-          }});
-		message.channel.send("Here's a normal message!") // Sends a message that isn't in an embed.
-		message.react('😎'); // The bot can also react to the user's message, with an emoji.
-    }
 
+    if(message.author.bot) return;
+        
+    if(!message.content.startsWith(config.prefix)) return;
+
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();  
+    if(command === ";kick") {
+        let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+        if(!kUser) return message.channel.send("Can't find user!");
+        let kReason = args.join(" ").slice(22);
+        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You are lacking the permissions `Manage Messages`!");
+        if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Thas person cannot be kicked!");
+
+        message.guild.member(kUser).kick(kReason)
+        message.channel.send("User was kicked!");
+
+        return;
+    }
+    if(command === ";ban") {
+        let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+        if(!bUser) return message.channel.send("Can't find user!");
+        let bReason = args.join(" ").slice(22);
+        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You are lacking the permissions `Manage Messages`!");
+        if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Thas person cannot be kicked!");
+
+        message.guild.member(bUser).ban(bReason)
+        message.channel.send("User was banned!");
+
+        return;
+    }
+    if(command === ";timeout") {
+        let tomute = message.mentions.members.first() || message.guild.members.get(args[0]);
+        if(!tomute) return message.reply("Couldn't find user.");
+        if(tomute.hasPermission("ADMINISTRATOR")) return message.reply("Can't mute them!");
+        let muterole = message.guild.roles.find(`name`, `Timeout`);
+        
+        let mutetime = args[1];
+        if(!mutetime) return message.reply("You didn't specify a time!");
+
+        await(tomute.addRole(muterole.id)); // If this doesn't work, try topit.roles.add(muterole.id);
+        message.reply(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
+
+        setTimeout(function(){
+            tomute.removeRole(muterole.id); // If this doesn't work, try topit.roles.remove(muterole.id);
+            message.channel.send(`<@${tomute.id}> has been unmuted!`)
+        }, ms(mutetime));
+    }
+    if(command === ";pit") {
+        let topit = message.mentions.members.first() || message.guild.members.get(args[0]);
+        if(!topit) return message.reply("Couldn't find user.");
+        if(topit.hasPermission("ADMINISTRATOR")) return message.reply("You can't sentence them!");
+        let pitrole = message.guild.roles.find(`name`, `Pit`);
+        
+        let pittime = args[1];
+        if(!pittime) return message.reply("You didn't specify a time!");
+
+        await(topit.addRole(pitrole.id)); // If this doesn't work, try topit.roles.add(pitrole.id);
+        message.reply(`<@${topit.id}> has been sentenced to the gulag for ${ms(ms(pittime))}`);
+
+        setTimeout(function(){
+            topit.removeRole(pitrole.id); // If this doesn't work, try topit.roles.remove(pitrole.id);
+            message.channel.send(`<@${topit.id}> has been released from the pit!`)
+        }, ms(pittime));
+    }
     })
 client.login(settings.botToken);
